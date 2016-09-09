5043eff5532fdcf605c632cce42bb61fe5f037b4
felipemrk2e <felipemrk2e@gmail.com>
2016-09-08 21:12:59 -0300

    Update imoveis.java

--- Projeto/src/Interface/Model/imoveis.java
+++ Projeto/src/Interface/Model/imoveis.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interface.Model;
+package Interface.CadImovel.backEnd;
 
 import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 import java.awt.event.WindowAdapter;
