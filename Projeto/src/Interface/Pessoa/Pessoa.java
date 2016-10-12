cac2ed63fb99e12428060435c70dd07923ceab6e
jeanfbd <jeanfbd@hotmail.com>
2016-10-11 21:59:39 -0300

    Criação PessoaDAO e exclusão de classes duplicadas

--- Projeto/src/Interface/Pessoa/Pessoa.java
+++ Projeto/src/Interface/Pessoa/Pessoa.java
@@ -5,12 +5,9 @@
  */
 package Interface.Pessoa;
 
+import global.model.Bairro;
+import global.model.Cidade;
 import global.model.Endereco;
-import imovel.model.Documentacao;
-import imovel.model.Imovel_has_TipoContrato;
-import imovel.model.Terreno;
-import imovel.model.TipoContrato;
-import imovel.model.TipoImovel;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,7 +19,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -33,18 +29,6 @@ import javax.persistence.OneToOne;
 @Entity
 public class Pessoa {
 
-    
-    private int numero;
-    private String complemento;
-    private String CEP;
-    private Bairro bairro;
-    private Cidade cidade;
-    private Estado estado;
-    private Pais pais;    
-    private String email;
-    private String observacoes;
-    private Date dataNascimento;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
@@ -52,90 +36,129 @@ public class Pessoa {
 
     @Column
     private String nomePessoa;
+
+    @Column
+    private String email;
+
+    @Column
+    private String observacoes;
+
+    @Column
+    private String dataNascimento;
     
-    
-    
-
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "idTelefone", nullable = false)
-    private Telefone telefone;
-
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "idEstadoCivil", nullable = true)
-    private EstadoCivil estadoCivil;
-
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "idTerreno", nullable = true)
-    private Terreno terreno;
-
     @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "idEndereco", nullable = true)
     private Endereco endereco;
-
-    //+addTipo
-    @OneToMany(
-            mappedBy = "imovel",
-            targetEntity = Imovel_has_TipoContrato.class,
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.PERSIST)
-    private List<Imovel_has_TipoContrato> tiposContratos = new ArrayList<>();
-
-//    public Imovel(int qtdQuartos, int qtdSuites, int qtdSalas, int qtdBanheiros, int qtdPisos,
-//            int lavanderia, int vagasGaragem, int areaServico, int piscina, int lavabos, int depEmpregados,
-//            String areaExterna, int dataConstrucao, String acabamento, String outrosItens, String descImovel,
-//            String observacoes, String chaves, int tipoMobilia, String descMobilia, double valorIptu,
-//            double valorCondominio, imovel.model.TipoImovel tipoImovel, imovel.model.Documentacao documentacao,
-//            imovel.model.Terreno terreno, global.model.Endereco endereco) {
-//        this.qtdQuartos = qtdQuartos;
-//        this.qtdSuites = qtdSuites;
-//        this.qtdSalas = qtdSalas;
-//        this.qtdBanheiros = qtdBanheiros;
-//        this.qtdPisos = qtdPisos;
-//        this.lavanderia = lavanderia;
-//        this.vagasGaragem = vagasGaragem;
-//        this.areaServico = areaServico;
-//        this.piscina = piscina;
-//        this.lavabos = lavabos;
-//        this.depEmpregados = depEmpregados;
-//        this.areaExterna = areaExterna;
-//        this.dataConstrucao = dataConstrucao;
-//        this.acabamento = acabamento;
-//        this.outrosItens = outrosItens;
-//        this.descImovel = descImovel;
-//        this.observacoes = observacoes;
-//        this.chaves = chaves;
-//        this.tipoMobilia = tipoMobilia;
-//        this.descMobilia = descMobilia;
-//        this.valorIptu = valorIptu;
-//        this.valorCondominio = valorCondominio;
-//        this.tipoImovel = tipoImovel;
-//        this.documentacao = documentacao;
-//        this.terreno = terreno;
-//        this.endereco = endereco;
-//    }
-
-//    public Imovel() {
-//
-//    }
-
-   
-
+    
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idBairro", nullable = true)
+    private Bairro bairro;
+    
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idCidade", nullable = true)
+    private Cidade cidade;
     
 
-//    public void addTipoContrato(TipoContrato tipoContrato, double valor) {
-//        Imovel_has_TipoContrato imovelTipoContrato = new Imovel_has_TipoContrato(this, tipoContrato, valor);
-//        tiposContratos.add(imovelTipoContrato);
-//        tipoContrato.getTiposContratos().add(imovelTipoContrato);
-//    }
-//
-//    public void removetipoContrato(TipoContrato tipoContrato) {
-//        Imovel_has_TipoContrato imovelTipoContrato = new Imovel_has_TipoContrato(this, tipoContrato, 0);
-//        tipoContrato.getTiposContratos().remove(imovelTipoContrato);
-//        tiposContratos.remove(imovelTipoContrato);
-//        imovelTipoContrato.setImovel(null);
-//        imovelTipoContrato.setTipoContrato(null);
-//        imovelTipoContrato.setValor(0);
-//    }
+    @OneToMany(
+            mappedBy = "telefone",
+            targetEntity = Telefone.class,
+            fetch = FetchType.LAZY)
+    private List<Telefone> telefone = new ArrayList<Telefone>();    
 
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idPessoaFisica", nullable = true)
+    private PessoaFisica pessoaFisica;
 
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "idPessoaJuridica", nullable = true)
+    private PessoaJuridica pessoaJuridica;
+
+   
+    public Pessoa() {
+
+    }
+
+    public Pessoa(int idPessoa, String nomePessoa, String email, List<Telefone> telefone, String observacoes, Date dataNascimento, Endereco endereco, Bairro bairro, Cidade cidade) {
+        this.idPessoa = idPessoa;
+        this.nomePessoa = nomePessoa;
+        this.email = email;
+        this.telefone = telefone;
+        this.observacoes = observacoes;
+        this.dataNascimento = this.dataNascimento;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+    }
+
+    public long getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(long idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+
+    public void setNomePessoa(String nomePessoa) {
+        this.nomePessoa = nomePessoa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public PessoaFisica getPessoaFisica() {
+        return pessoaFisica;
+    }
+
+    public void setPessoaFisica(PessoaFisica pessoaFisica) {
+        this.pessoaFisica = pessoaFisica;
+    }
+
+    public PessoaJuridica getPessoaJuridica() {
+        return pessoaJuridica;
+    }
+
+    public void setPessoaJuridica(PessoaJuridica pessoaJuridica) {
+        this.pessoaJuridica = pessoaJuridica;
+    }
+    
+    public void addTelefone(Telefone telefone){
+        this.telefone.add(telefone);
+    }
+    
+    
+
+  
 }
