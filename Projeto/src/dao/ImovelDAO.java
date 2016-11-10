defd0594db5f0140c2b4894a65323b3dc536d90a
urielcaire <uriel.caire@gmail.com>
2016-11-10 15:40:49 -0200

    Adição do método de pesquisa searchImovel() ao ImovelDAO. Com este método é será possível algumas pesquisas básicas.

--- Projeto/src/dao/ImovelDAO.java
+++ Projeto/src/dao/ImovelDAO.java
@@ -36,5 +36,50 @@ public class ImovelDAO extends DAO<Imovel>{
     public List<Imovel> getAll() {
         return entityManager.createQuery("FROM Imovel").getResultList();
     }
+    
+    /**
+        * Retorna uma lista de Imoveis de acordo com a pesquisa.
+        * @param  idsTiposImovel Lista contendo os ids da tabela TipoImovel
+        * @param rua rua da tabela Endereco relacionado ao Imovel
+        * @param bairro bairro da tabela Endereco relacionado ao Imovel
+        * @param idCidade id da Cidade da tabela Endereco relacionado ao Imovel
+        * @param qtdQuartos quantidade de quartos do Imovel
+        * @param garagem quantidade de vagas da garagem em Imovel
+        * @return      lista de Imoveis ativos com base nas condições de pesquisa
+        */
+    public List<Imovel> searchImovel(List<Long> idsTiposImovel, String rua, String bairro, long idCidade, int qtdQuartos, int garagem){
+        String query = "";
+        String or = "";
+        if(idsTiposImovel.size() > 0){
+            for (int i = 0; i < idsTiposImovel.size(); i++) {
+                if(i==1){
+                    or = " or ";
+                }
+                query += or+"im.tipoImovel.idTipoImovel = "+idsTiposImovel.get(i);
+            }
+            or = " or ";
+        }
+        if(rua != ""){
+            query += or+"im.endereco.nomeEndereco LIKE '%"+rua+"%'";
+            or = " or ";
+        }
+        if(bairro != ""){
+            query += or+"im.endereco.bairro LIKE '%"+bairro+"%'";
+            or = " or ";
+        }
+        if(idCidade > 0){
+            query += or+"im.endereco.cidade.idCidade = "+idCidade;
+            or = " or ";
+        }
+        if(qtdQuartos > 0){
+            query += or+"im.qtdQuartos >= "+qtdQuartos;
+            or = " or ";
+        }
+        if(garagem > 0){
+            query += or+"im.vagasGaragem >= "+garagem;
+            or = " or ";
+        }
+        return entityManager.createQuery("FROM Imovel im WHERE "+query+" and im.ativo = 1").getResultList();
+    }
 
 }
